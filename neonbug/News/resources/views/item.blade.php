3042d4f1f0785492ba52360fd4a33843eaa45bad
Tadej Kanižar <tadej@ncode.si>
2015-09-07 12:39:02 +0200

    Added main_image output to default News templates

--- neonbug/News/resources/views/item.blade.php
+++ neonbug/News/resources/views/item.blade.php
@@ -1,6 +1,12 @@
 <h1>{{ $item->title }}</h1>
 <strong>{{ date('d.m.Y', strtotime($item->published_from_date)) }}</strong>
 
+@if ($item->main_image != null && $item->main_image != '')
+<div>
+	<img src="{!! Croppa::url('uploads/news/' . $item->main_image, 150, 150) !!}" />
+</div>
+@endif
+
 <div>
 	{!! $item->contents !!}
 </div>
