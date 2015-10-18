f1c21078650fb6ea2e63ce5c156c5f0aef20c2cf
Tadej Kanižar <tadej@ncode.si>
2015-10-18 19:31:27 +0200

    Added missing Text translations

--- neonbug/Text/database/migrations/translations.text/admin.php
+++ neonbug/Text/database/migrations/translations.text/admin.php
@@ -1,10 +1,22 @@
-<?php return [
-	'text::admin.title.main' => [ 'en' => 'Texts', 'sl' => 'Vsebina' ], 
-	'text::admin.title.list' => [ 'en' => 'List', 'sl' => 'Seznam' ], 
-	'text::admin.title.add'  => [ 'en' => 'Add', 'sl' => 'Dodaj' ], 
-	'text::admin.title.edit' => [ 'en' => 'Edit', 'sl' => 'Uredi' ], 
+<?php
+$p = 'text::admin.';
+return [
+	$p . 'title.main' => [ 'en' => 'Texts', 'sl' => 'Vsebina' ], 
+	$p . 'title.list' => [ 'en' => 'List', 'sl' => 'Seznam' ], 
+	$p . 'title.add'  => [ 'en' => 'Add', 'sl' => 'Dodaj' ], 
+	$p . 'title.edit' => [ 'en' => 'Edit', 'sl' => 'Uredi' ], 
 	
-	'text::admin.menu.main' => [ 'en' => 'Texts', 'sl' => 'Vsebina' ], 
-	'text::admin.menu.list' => [ 'en' => 'List', 'sl' => 'Seznam' ], 
-	'text::admin.menu.add'  => [ 'en' => 'Add', 'sl' => 'Dodaj' ], 
+	$p . 'menu.main' => [ 'en' => 'Texts', 'sl' => 'Vsebina' ], 
+	$p . 'menu.list' => [ 'en' => 'List', 'sl' => 'Seznam' ], 
+	$p . 'menu.add'  => [ 'en' => 'Add', 'sl' => 'Dodaj' ], 
+	
+	$p . 'list.field-title.id_text'    => [ 'en' => 'Id',      'sl' => 'Id' ], 
+	$p . 'list.field-title.title'      => [ 'en' => 'Title',   'sl' => 'Naziv' ], 
+	$p . 'list.field-title.slug'       => [ 'en' => 'Url',     'sl' => 'Url' ], 
+	$p . 'list.field-title.updated_at' => [ 'en' => 'Updated', 'sl' => 'Posodobljeno' ], 
+	
+	$p . 'add.field-title.title'      => [ 'en' => 'Title',    'sl' => 'Naziv' ], 
+	$p . 'add.field-title.slug'       => [ 'en' => 'Url',      'sl' => 'Url' ], 
+	$p . 'add.field-title.main_image' => [ 'en' => 'Image',    'sl' => 'Slika' ], 
+	$p . 'add.field-title.contents'   => [ 'en' => 'Contents', 'sl' => 'Vsebina' ], 
 ];
