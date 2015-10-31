134ad8c1773315a9b4cfab40e646e4d5316c569f
Tadej Kanižar <tadej@ncode.si>
2015-10-31 13:04:46 +0100

    Implemented better default frontend views for Gallery

--- neonbug/Gallery/resources/views/index.blade.php
+++ neonbug/Gallery/resources/views/index.blade.php
@@ -3,8 +3,16 @@
 @foreach ($items as $item)
 	<?php if (mb_strlen($item->slug) == 0) continue; ?>
 	
-	<div>
-		<h2><a href="{{ route('gallery::slug::' . $item->slug) }}">{{ $item->{$item->getKeyName()} }}</a></h2>
+	<a href="{{ route('gallery::slug::' . $item->slug) }}" style="display: block;">
+		<div style="border: 1px solid black; width: 150px; height: 150px; float: left; margin-right: 12px;">
+			@if ($item->main_image != null && $item->main_image != '')
+				<img src="{!! Croppa::url('uploads/gallery/' . $item->main_image, 150, 150) !!}" />
+			@endif
+		</div>
+		
+		<h2>{{ $item->title }}</h2>
 		<strong>{{ date('d.m.Y', strtotime($item->updated_at)) }}</strong>
-	</div>
+		
+		<div style="clear: both;"></div>
+	</a>
 @endforeach
