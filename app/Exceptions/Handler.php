f8a28cdb697e39e744bd034a52a967efafe7fd13
Tadej Kanižar <tadej@ncode.si>
2016-03-08 21:12:54 +0100

    Fix for PHP 7 exceptions (throwables)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,7 @@
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler {
+class Handler extends \Neonbug\Common\Exceptions\Handler {
 
 	/**
 	 * A list of the exception types that should not be reported.
@@ -19,10 +19,10 @@ class Handler extends ExceptionHandler {
 	 *
 	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
 	 *
-	 * @param  \Exception  $e
+	 * @param  \Throwable  $e
 	 * @return void
 	 */
-	public function report(Exception $e)
+	public function report($e)
 	{
 		return parent::report($e);
 	}
@@ -31,10 +31,10 @@ class Handler extends ExceptionHandler {
 	 * Render an exception into an HTTP response.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
+	 * @param  \Throwable  $e
 	 * @return \Illuminate\Http\Response
 	 */
-	public function render($request, Exception $e)
+	public function render($request, $e)
 	{
 		return parent::render($request, $e);
 	}
