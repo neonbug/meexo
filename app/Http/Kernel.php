c6bfb1c83caab156427d5781c571b89fb9341cda
Tadej Kanižar <tadej@ncode.si>
2020-04-26 23:36:14 +0200

    Update to Laravel 6.0

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel {
 
 		'api' => [
 			'throttle:60,1',
-			'bindings',
+			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 		],
 	];
 
@@ -59,6 +59,24 @@ class Kernel extends HttpKernel {
 		'auth.admin'    => \Neonbug\Common\Http\Middleware\AuthenticateAdmin::class,
 		'admin.menu'    => \Neonbug\Common\Http\Middleware\AdminMenu::class,
 		'online'        => \Neonbug\Common\Http\Middleware\Online::class,
+		'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+	];
+
+	/**
+	 * The priority-sorted list of middleware.
+	 *
+	 * This forces non-global middleware to always be in the given order.
+	 *
+	 * @var array
+	 */
+	protected $middlewarePriority = [
+		\Illuminate\Session\Middleware\StartSession::class,
+		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+		\App\Http\Middleware\Authenticate::class,
+		\Illuminate\Routing\Middleware\ThrottleRequests::class,
+		\Illuminate\Session\Middleware\AuthenticateSession::class,
+		\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		\Illuminate\Auth\Middleware\Authorize::class,
 	];
 
 }
